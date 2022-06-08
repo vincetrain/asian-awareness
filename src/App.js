d0e76b8b59810f4d9ef5ce2c9b278f4f4762dc2e
Vincent Tran <tran.vincent@outlook.com>
2022-06-08 14:43:11 -0400

    Updated transitions with React-Transition-Group

--- src/App.js
+++ src/App.js
@@ -8,35 +8,35 @@ import History from "./pages/History.jsx";
 import Resources from "./pages/Resources.jsx";
 import Help from "./pages/Help.jsx";
 
-import { Routes, Route } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
-const animations = {
-    initial: {opacity: 0},
-    animate: {opacity: 1, x: 0},
-    exit: {opacity: 0}
+function Content() {
+    let location = useLocation();
+    return(
+        <div>
+            <TransitionGroup>
+                <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
+                    <Routes location={location}>
+                        <Route exact path="/" element={<Home />} />
+                        <Route path="/Summary" element={<Summary />} />
+                        <Route path="/History" element={<Home />} />
+                        <Route path="/Resources" element={<Home />} />
+                        <Route path="/Help" element={<Home />} />
+                    </Routes>
+                </CSSTransition>
+            </TransitionGroup>
+        </div>
+    )
+
+
 }
 
 function App() {
     return (
         <div>
             <Navbar />
-
-            <motion.div
-            variants={animations}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            transition={{duration: 0.6}}
-            >
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/Summary" element={<Summary />} />
-                    <Route path="/History" element={<Home />} />
-                    <Route path="/Resources" element={<Home />} />
-                    <Route path="/Help" element={<Home />} />
-                </Routes>
-            </motion.div>
+            <Content />
         </div>
     )
 }
